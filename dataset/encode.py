80b6fef3ef642a9f226bda4f69ec9ca1c57b1b4c
tarepan <tarepan5884@gmail.com>
2023-05-10 06:55:04 +0000

    Refactor runners

--- dataset/encode.py
+++ dataset/encode.py
@@ -1,59 +1,43 @@
-import argparse
-import logging
-import numpy as np
-from pathlib import Path
-from tqdm import tqdm
-import os
+"""Preprocessing"""
 
+import argparse
+from pathlib import Path
+
+import numpy as np
 import torch
 import torchaudio
 from torchaudio.functional import resample
+from tqdm import tqdm
 
 
 def encode_dataset(args):
+    """Preprocess"""
     print(f"Loading hubert checkpoint")
     hubert = torch.hub.load("bshall/hubert:main", f"hubert_soft").cuda().eval()
 
     print(f"Encoding dataset at {args.in_dir}")
+    # All audio files under the `in_dir`
     for in_path in tqdm(list(args.in_dir.rglob(f"*{args.extension}"))):
         out_path = args.out_dir / in_path.relative_to(args.in_dir)
         out_path.parent.mkdir(parents=True, exist_ok=True)
-        if True:#not os.path.exists(out_path.with_suffix(".npy")):
-            wav, sr = torchaudio.load(in_path)
-            wav = resample(wav, sr, 16000)
-            wav = wav.unsqueeze(0).cuda()
 
-            with torch.inference_mode():
-                units = hubert.units(wav)
+        wav, sr = torchaudio.load(in_path)
+        wav = resample(wav, sr, 16000)
+        wav = wav.unsqueeze(0).cuda()
 
-            
-            np.save(out_path.with_suffix(".npy"), units.squeeze().cpu().numpy())
+        # Wave-to-Unit
+        with torch.inference_mode():
+            units = hubert.units(wav)
+
+        # Save - saved in same relative path with .npy suffix
+        np.save(out_path.with_suffix(".npy"), units.squeeze().cpu().numpy())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Encode an audio dataset.")
-    parser.add_argument(
-        "model",
-        help="available models (HuBERT-Soft or HuBERT-Discrete)",
-        choices=["soft", "soft"],
-    )
-    parser.add_argument(
-        "in_dir",
-        metavar="in-dir",
-        help="path to the dataset directory.",
-        type=Path,
-    )
-    parser.add_argument(
-        "out_dir",
-        metavar="out-dir",
-        help="path to the output directory.",
-        type=Path,
-    )
-    parser.add_argument(
-        "--extension",
-        help="extension of the audio files (defaults to .flac).",
-        default=".wav",
-        type=str,
-    )
+    parser.add_argument("model",                      help="available models (HuBERT-Soft or HuBERT-Discrete)", choices=["soft", "soft"])
+    parser.add_argument("in_dir",  metavar="in-dir",  help="path to the dataset directory.",                 type=Path)
+    parser.add_argument("out_dir", metavar="out-dir", help="path to the output directory.",                  type=Path)
+    parser.add_argument("--extension",                help="extension of the audio files.",  default=".wav", type=str)
     args = parser.parse_args()
     encode_dataset(args)
