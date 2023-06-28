d1a1e7c1f1dbc19b261cb75c3206251fb10638da
tarepan <tarepan5884@gmail.com>
2023-06-28 13:17:17 +0000

    Fix dataloader workers

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import itertools
 import math
+from multiprocessing import cpu_count
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -77,11 +78,11 @@ def run(rank, n_gpus, hps):
       rank=rank,
       shuffle=True)
   collate_fn = TextAudioSpeakerCollate(hps)
-  train_loader = DataLoader(train_dataset, num_workers=8, shuffle=False, pin_memory=True,
+  train_loader = DataLoader(train_dataset, num_workers=cpu_count(), shuffle=False, pin_memory=True,
       collate_fn=collate_fn, batch_sampler=train_sampler)
   if rank == 0:
     eval_dataset = TextAudioSpeakerLoader(hps.data.validation_files, hps)
-    eval_loader = DataLoader(eval_dataset, num_workers=8, shuffle=True,
+    eval_loader = DataLoader(eval_dataset, num_workers=cpu_count(), shuffle=True,
         batch_size=1, pin_memory=False,
         drop_last=False)
   
