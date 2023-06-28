Fix KL loss

losses.py
@@ -53,7 +53,7 @@ def generator_loss(disc_outputs: list[Tensor]) -> tuple[Tensor, list[Tensor]]:
     return loss, gen_losses
 
 
-def kl_loss(z_p: Tensor, logs_q: Tensor, m_p: Tensor, logs_p: Tensor) -> Tensor:
+def kl_loss(z_p: Tensor, logs_q: Tensor, m_p: Tensor, logs_p: Tensor, z_mask: Tensor) -> Tensor:
     """
     KL divergence loss.
 
@@ -66,6 +66,8 @@ def kl_loss(z_p: Tensor, logs_q: Tensor, m_p: Tensor, logs_p: Tensor) -> Tensor:
     kl = logs_p - logs_q - 0.5
     kl += 0.5 * ((z_p - m_p)**2) * torch.exp(-2. * logs_p)
     l = torch.mean(kl)
+    kl = torch.sum(kl * z_mask)
+    l = kl / torch.sum(z_mask)
 
     return l
 